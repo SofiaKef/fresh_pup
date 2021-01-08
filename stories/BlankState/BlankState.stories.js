b65c2fcd52a8f32e59407f452a9352ee61514ec5
SofiaKef <68301861+SofiaKef@users.noreply.github.com>
2021-01-08 17:14:09 +0200

    Storybook: BlankState & Content

---
+++ stories/BlankState/BlankState.stories.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import BlankState from './BlankState';
+
+export default {
+  title: 'Components/Blank State',
+  component: BlankState,
+};
+
+const Template = (args) => <BlankState {...args} />;
+
+const doNothing = () => console.log('does nothing');
+
+export const Documents = Template.bind({});
+Documents.args = {
+  icon: { style: 'solid', symbol: 'file-alt' },
+  title: "You're plum out of documents, friend!",
+  subtitle: 'Add your first document by clicking the button below.',
+  action: {
+    style: 'success',
+    onClick: doNothing,
+    label: 'Create Your First Document',
+  },
+};
+
+export const AdminUserSettings = Template.bind({});
+AdminUserSettings.args = {
+  icon: { style: 'solid', symbol: 'gear' },
+  title: 'No user settings here, friend.',
+  subtitle: 'Add your first setting by clicking the button below.',
+  action: {
+    style: 'success',
+    onClick: doNothing,
+    label: 'Create Your First Setting',
+  },
+};
